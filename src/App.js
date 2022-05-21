0cd2b9d95b1bbed9947b8e7909c8572b7a829eff
Paula Yamashita <pymoura@gmail.com>
2022-05-20 23:04:04 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = 5;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {a}
         </a>
       </header>
     </div>
